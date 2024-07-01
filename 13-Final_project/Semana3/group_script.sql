e8d5a8b9c1d311414dd1a54b2cd3ea4e4ac73dba
daiane0 <daianemacedo001@gmail.com>
2024-07-01 17:53:00 -0300

    .

--- 13-Final_project/Semana3/group_script.sql
+++
@@ -1,8 +0,0 @@
-SELECT country, category, sum(price) as "Total_sales"
-from softcart_factsales
-left join softcart_dimcountry 
-on softcart_factsales.country_id = softcart_dimcountry.country_id
-left join softcart_dimcategory
-on softcart_factsales.category_id = softcart_dimcategory.category_id
-group by grouping sets(country, category)
-order by country, category
